da1aea13e2281aedd5c270bf371eaaacf5675db4
Ana Carolina dos Santos Silva <ana.carolinasantos@icloud.com>
2018-04-05 23:26:09 -0300

    foda-se

--- src/MiniVisuAlgCompiler.c
+++ src/MiniVisuAlgCompiler.c
@@ -11,8 +11,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAXBUFFERLENGTH 1000
+#define MAXVARIABLES 1000
 
 typedef struct {
 	char* classification;
@@ -23,11 +25,13 @@ static FILE *source_file, *destination_file;
 
 const size_t bufferSize = MAXBUFFERLENGTH;
 
-static int begin = 0, forward = 0;
+static int begin = 0, forward = 0, line = 0, indexVariables = 0;
 
 static char *buffer;
 
 static int isStringLiteral = 0;
+
+static char *variables[MAXVARIABLES];
 
 void openFile(FILE **file, char fileName[], char type[]) {
 	*file = fopen(fileName, type);
@@ -49,25 +53,33 @@ char getNextChar(){
 
 Token* getToken() {
 	Token *token = (Token *) calloc(255,sizeof(Token));
-	char c;
-	char nextC;
+	char c, nextC, result[12];
+	int reachSpace = 0, i, foundId = 0;
+	char* word;
 
 	c = buffer[forward];
+
+	for(i = 0; i < MAXVARIABLES; i++) {
+		variables[i] = "";
+	}
+
 
 	if (c == ' ') {
 		begin++;
 		forward = begin;
 
 		c = buffer[forward];
+	} else if (c == '\n') {
+		return NULL;
 	}
 
-	printf("caracter %c\n", c);
+	printf("c %c\n", c);
 
 	if (isStringLiteral) {
 		if (c == '"') {
+			isStringLiteral = 0;
 			token->classification = "text";
 			token->attribute = "";
-			isStringLiteral = 0;
 		}
 	} else {
 		switch (c) {
@@ -132,7 +144,9 @@ Token* getToken() {
 			}
 			break;
 		case '>':
-			if (getNextChar() == '=') {
+			nextC = getNextChar();
+
+			if (nextC == '=') {
 				token->classification = "rel-op";
 				token->attribute = "GE";
 			} else {
@@ -146,6 +160,113 @@ Token* getToken() {
 			token->attribute = "EQ";
 			break;
 		default:
+//			if (c == ' ' || c == '\n') {
+//				return NULL;
+//			}
+			reachSpace = 0;
+			word = &buffer[begin];
+			while(!reachSpace) {
+				word[forward - begin] = buffer[forward];
+
+				if (getNextChar() == ' ' || getNextChar() == '\n') {
+					word[forward - begin] = '\0';
+					reachSpace = 1;
+				}
+			}
+
+			if (strcmp(word, "algoritmo") == 0) {
+				token->classification = "algoritmo";
+				token->attribute = "";
+			} else if (strcmp(word, "var") == 0) {
+				token->classification = "var";
+				token->attribute = "";
+			} else if (strcmp(word, "inteiro") == 0) {
+				token->classification = "inteiro";
+				token->attribute = "";
+			} else if (strcmp(word, "inicio") == 0) {
+				token->classification = "inicio";
+				token->attribute = "";
+			} else if (strcmp(word, "fimalgoritmo") == 0) {
+				token->classification = "fimalgoritmo";
+				token->attribute = "";
+			} else if (strcmp(word, "logico") == 0) {
+				token->classification = "logico";
+				token->attribute = "";
+			} else if (strcmp(word, "verdadeiro") == 0) {
+				token->classification = "verdadeiro";
+				token->attribute = "";
+			} else if (strcmp(word, "falso") == 0) {
+				token->classification = "falso";
+				token->attribute = "";
+			} else if (strcmp(word, "mod") == 0) {
+				token->classification = "arith-op";
+				token->attribute = "MOD";
+			} else if (strcmp(word, "exp") == 0) {
+				token->classification = "arith-op";
+				token->attribute = "EXP";
+			} else if (strcmp(word, "leia") == 0) {
+				token->classification = "leia";
+				token->attribute = "";
+			} else if (strcmp(word, "escreva") == 0) {
+				token->classification = "escreva";
+				token->attribute = "";
+			} else if (strcmp(word, "se") == 0) {
+				token->classification = "se";
+				token->attribute = "";
+			} else if (strcmp(word, "entao") == 0) {
+				token->classification = "entao";
+				token->attribute = "";
+			} else if (strcmp(word, "senao") == 0) {
+				token->classification = "senao";
+				token->attribute = "";
+			} else if (strcmp(word, "fimse") == 0) {
+				token->classification = "fimse";
+				token->attribute = "";
+			} else if (strcmp(word, "para") == 0) {
+				token->classification = "para";
+				token->attribute = "";
+			} else if (strcmp(word, "de") == 0) {
+				token->classification = "de";
+				token->attribute = "";
+			} else if (strcmp(word, "ate") == 0) {
+				token->classification = "ate";
+				token->attribute = "";
+			} else if (strcmp(word, "fimpara") == 0) {
+				token->classification = "fimpara";
+				token->attribute = "";
+			} else if (strcmp(word, "enquanto") == 0) {
+				token->classification = "enquanto";
+				token->attribute = "";
+			} else if (strcmp(word, "faca") == 0) {
+				token->classification = "faca";
+				token->attribute = "";
+			} else if (strcmp(word, "fimenquanto") == 0) {
+				token->classification = "fimenquanto";
+				token->attribute = "";
+			} else {
+				printf("VARIAAAAAAAAAAAVEL");
+				foundId = 0;
+				for (i = 0; i < MAXVARIABLES; i++) {
+					if (strcmp(word, variables[i]) == 0) {
+						token->classification = "id";
+					    sprintf(result, "%d", i);
+						token->attribute = result;
+						foundId = 1;
+						break;
+					}
+				}
+				if (!foundId) {
+					variables[indexVariables] = word;
+					token->classification = "id";
+					sprintf(result, "%d", indexVariables);
+					token->attribute = result;
+					indexVariables++;
+				}
+			}
+
+			printf("PALAVR: %s \n", word);
+
+
 			//TODO: tratar variaveis (armazenar numa matriz/vetor), palavra reservada e numeros
 			printf("palavra ou numero ");
 			break;
@@ -171,10 +292,18 @@ void readFile() {
 
 			t = getToken();
 
+			if (t != NULL && t->classification != NULL) {
+				if (t->attribute == NULL) {
+					fprintf(destination_file, "<%s>\n", t->classification);
+				} else {
+					fprintf(destination_file, "<%s, %s>\n", t->classification, t->attribute);
+				}
+			}
 			printf("%s %s\n", t->classification, t->attribute);
 
 			begin = forward;
 		}
+		line++;
 	}
 }
 
